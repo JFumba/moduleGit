b243080ed49686b94c8aa932115c1ab1be05e3bc
Elisa Sertyn <minkoelisa@gmail.com>
2020-07-08 14:30:08 +0200

    point Elisa

---
+++ GIT/Bien.java
@@ -0,0 +1,17 @@
+package GIT;
+
+public class Bien {
+    private float prix;
+
+    public Bien(float prix) {
+        setPrix(prix);
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+}
