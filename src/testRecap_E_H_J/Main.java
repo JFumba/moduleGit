031d882d5e9646aeadfd4d9951f753516e870bf4
Jesse Fumba <jessefumba@gmail.com>
2020-07-08 15:32:16 +0200

    ajustement vetement

--- src/testRecap_E_H_J/Main.java
+++ src/testRecap_E_H_J/Main.java
@@ -36,7 +36,7 @@ public class Main {
         habitant3.ajouterBiens(livre3);
         habitant4.ajouterBiens(livre4);
 */
-        Vetement test = new Vetement(12.0F , "v");
+        Vetement test = new Vetement(12.0F , "S");
         System.out.println(test);
         System.out.println("test");
     }
